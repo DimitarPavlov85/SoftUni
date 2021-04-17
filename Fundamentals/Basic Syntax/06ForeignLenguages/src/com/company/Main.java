afe50ffdee0bf8dbd0c6654c62d23b0305df30ad
mitko851111 <mitko851111@abv.bg>
2021-04-17 16:58:09 +0300

    PrintLenguages

---
+++ Fundamentals/Basic Syntax/06ForeignLenguages/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner((System.in));
+
+        String namecountry=sc.next();
+
+        if( namecountry.equals("USA")  || namecountry.equals("England")){
+            System.out.print("English");
+        }
+        else if(namecountry.equals("Spain")||namecountry.equals("Argentina")||namecountry.equals("Mexico")){
+            System.out.print("Spanish");
+        }
+        else {
+            System.out.print("unknown");
+        }
+    }
+}

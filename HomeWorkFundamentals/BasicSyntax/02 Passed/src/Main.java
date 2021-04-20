83b181fccb9c260990191fdd45a86389bec915f2
mitko851111 <mitko851111@abv.bg>
2021-04-20 13:32:57 +0300

    Add Task 02 Passed

---
+++ HomeWorkFundamentals/BasicSyntax/02 Passed/src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float grade = Float.parseFloat(sc.nextLine());
+        if (grade >= 3.00) {
+            System.out.print("Passed!");
+        }
+    }
+}

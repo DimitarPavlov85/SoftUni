6b09f62d64544c916eb9b4686fcc630f7392edc6
mitko851111 <mitko851111@abv.bg>
2021-04-21 17:35:13 +0300

    New Task from Data Types

---
+++ HomeWorkFundamentals/Data Types and Variables/01 Integer Operations/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner((System.in));
+        int firstnumber=Integer.parseInt(sc.nextLine());
+        int secondnumber=Integer.parseInt(sc.nextLine());
+        int thurdnumber=Integer.parseInt(sc.nextLine());
+        int fourthnumber=Integer.parseInt(sc.nextLine());
+        int result=fourthnumber*((firstnumber+secondnumber) /thurdnumber);
+        System.out.print(result);
+    }
+}

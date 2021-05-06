7246aa6cbef22a706b44cac013b4d50e3a12651c
mitko851111 <mitko851111@abv.bg>
2021-05-06 19:18:07 +0300

    Add Task List Operations

---
+++ HomeWorkFundamentals/List/04 List Operations/src/Main.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void PrintList(List<Integer> Current) {
+        for (int i = 0; i < Current.size(); i++) {
+            System.out.print(Current.get(i) + " ");
+        }
+    }
+
+    private static List<Integer> CreateList(Scanner sc) {
+        List<Integer> Digits = new ArrayList<>();
+        String line = sc.nextLine();
+        String[] LineOfDigits = line.split(" ");
+        for (int i = 0; i < LineOfDigits.length; i++) {
+            Digits.add(Integer.parseInt(LineOfDigits[i]));
+        }
+        return Digits;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> Digitts = CreateList(sc);
+        String CommandsAndValues = sc.nextLine();
+        while (!CommandsAndValues.equals("End")) {
+            String[] Commands = CommandsAndValues.split(" ");
+
+            if (Commands[0].equals("Add")) {
+                int DigitToAdd = Integer.parseInt(Commands[1]);
+                Digitts.add(Digitts.size(), DigitToAdd);
+            } else if (Commands[0].equals("Remove")) {
+                int Index = Integer.parseInt(Commands[1]);
+                if (Index > Digitts.size() - 1) {
+                    System.out.print("Invalid index\n");
+                } else {
+                    Digitts.remove(Index);
+                }
+            } else if (Commands[0].equals("Insert")) {
+                int Index = Integer.parseInt(Commands[2]);
+                if (Index > Digitts.size()) {
+                    System.out.print("Invalid index");
+                } else {
+                    int Value = Integer.parseInt(Commands[1]);
+                    Digitts.add(Index, Value);
+                }
+            } else if (Commands[0].equals("Shift")) {
+                int Index = Integer.parseInt(Commands[2]);
+
+                if (Index > Digitts.size()) {
+                    System.out.print("Invalid index\n");
+
+                } else if (Commands[1].equals("left")) {
+                    for (int i = 0; i < Index; i++) {
+                        Digitts.add(Digitts.size(), Digitts.get(i));
+                    }
+
+                    while (0 < Index) {
+                        Digitts.remove(0);
+                        Index--;
+                    }
+                } else if (Commands[1].equals("right")) {
+                    Index = Digitts.size() - Index;
+                    for (int i = Index; i < Digitts.size(); i++) {
+                        int currentdigit = Digitts.get(i);
+                        Digitts.remove(i);
+                        Digitts.add(0, currentdigit);
+                    }
+                }
+            }
+            CommandsAndValues = sc.nextLine();
+        }
+        PrintList(Digitts);
+    }
+}

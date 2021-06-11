6110eb7aad649119cf7156dff74bb7a9b9157276
mitko851111 <mitko851111@abv.bg>
2021-06-11 18:03:20 +0300

    Add task 01

---
+++ Advanced/Defining Classes/01 Opinion Poll/src/Main.java
@@ -0,0 +1,27 @@
+
+import javafx.print.Collation;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = Integer.parseInt(sc.nextLine());
+        List<People> list = new ArrayList<>();
+
+        for (int i = 0; i < number; i++) {
+            String[] data = sc.nextLine().split(" ");
+            String currentName = data[0];
+            int currentAge = Integer.parseInt(data[1]);
+            if (currentAge > 30) {
+                People current = new People(currentName, currentAge);
+                list.add(current);
+            }
+        }
+
+        Collections.sort(list,Comparator.comparing(People::getName));
+        list.forEach(e-> System.out.println(e.getName()+ " - "+e.getAge()));
+    }
+}

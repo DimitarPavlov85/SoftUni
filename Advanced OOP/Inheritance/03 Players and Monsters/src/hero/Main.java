4b1b9045b8dcd32f9701c030f362f2b27910014a
mitko851111 <mitko851111@abv.bg>
2021-07-25 23:31:01 +0300

    Add Task 03

---
+++ Advanced OOP/Inheritance/03 Players and Monsters/src/hero/Main.java
@@ -0,0 +1,10 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+       String name="Mitko";
+       int level=5;
+        MuseElf f=new MuseElf(name ,level);
+        System.out.println(f.toString());
+    }
+}

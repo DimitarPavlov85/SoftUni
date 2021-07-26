8525f681b5306fc5a9953f0a73db196e313528b5
mitko851111 <mitko851111@abv.bg>
2021-07-26 09:45:41 +0300

    Add Task 05

---
+++ Advanced OOP/Inheritance/05 Restaurant/src/restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage{
+    public ColdBeverage(String name, BigDecimal price,double mililiters){
+        super(name,price,mililiters);
+    }
+}
---
+++ Advanced OOP/Inheritance/05 Restaurant/src/restaurant/Main.java
@@ -0,0 +1,11 @@
+package restaurant;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+      
+    }
+}
